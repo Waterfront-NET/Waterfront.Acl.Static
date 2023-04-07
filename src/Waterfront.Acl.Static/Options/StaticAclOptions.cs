48e0d4546057148d2e454b38ddc1d5878c03de80
2CHEVSKII <prsroman3@gmail.com>
2023-04-07 15:06:33 +0300

    Remove old project files

--- src/Waterfront.Acl.Static/Options/StaticAclOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Waterfront.Acl.Static.Models;
-
-namespace Waterfront.Acl.Static.Options;
-
-public class StaticAclOptions
-{
-    public StaticAclUser[] Users { get; set; }
-    public StaticAclPolicy[] Acl { get; set; }
-}
